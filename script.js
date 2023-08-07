0370e4112b4185b5bf67c1098ab011a4662e783a
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-08-07 15:16:11 -0700

    fixed the buttons

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ window.onload = function() {
 };
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 const body = document.querySelector("body");
+// const main = document.querySelector("main");
+// const footer = document.querySelector("footer");
 
 darkModeToggle.onclick = function() {
   body.classList.toggle("dark-mode");
-  
+  // main.classList.toggle("dark-mode");
+  // footer.classList.toggle("dark-mode");
 };

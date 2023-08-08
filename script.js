fdf41c9513e68f3cc48932257c60a9af83d94ab7
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-08-08 00:14:19 -0700

    added pictures

--- script.js
+++ script.js
@@ -8,11 +8,27 @@ window.onload = function() {
 };
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 const body = document.querySelector("body");
-// const main = document.querySelector("main");
-// const footer = document.querySelector("footer");
 
 darkModeToggle.onclick = function() {
   body.classList.toggle("dark-mode");
-  // main.classList.toggle("dark-mode");
-  // footer.classList.toggle("dark-mode");
 };
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
